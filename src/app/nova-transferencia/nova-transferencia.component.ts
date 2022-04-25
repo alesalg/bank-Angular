e157ee55c90e9a0a66b3f5fabafe6344e885f0a1
alesalg <allesalg@gmail.com>
2022-04-25 15:43:44 -0300

    add limpar campos e interpolação dos campos digitados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -16,6 +16,12 @@ export class NovaTransferenciaComponent implements OnInit {
     console.log('Solicitada nova transferência');
     const valorEmitir = {valor: this.valor, destino: this.destino};
     this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 
   constructor() { }
